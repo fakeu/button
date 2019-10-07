f9c130a5aa85a4b33f1fa93e74ebaf6b4b843e06
Anton Kislyi <fakeu@MacBook-Pro-Fakeu.local>
2019-10-07 13:24:56 +0300

    Updates

---
+++ precache-manifest.1a073bcb072d2a13926481c3219cce88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64f47a8415991a68ece7f0385c67c58",
+    "url": "/button/index.html"
+  },
+  {
+    "revision": "70c14edb3274c4ba4e66",
+    "url": "/button/static/css/main.82a4f70c.chunk.css"
+  },
+  {
+    "revision": "a690da32cff2d93c3392",
+    "url": "/button/static/js/2.332c11a5.chunk.js"
+  },
+  {
+    "revision": "70c14edb3274c4ba4e66",
+    "url": "/button/static/js/main.30b31881.chunk.js"
+  },
+  {
+    "revision": "6925e88d70258a2da75c",
+    "url": "/button/static/js/runtime-main.61c87145.js"
+  },
+  {
+    "revision": "420a89656e5ead92263dcfcdd5b64ad7",
+    "url": "/button/static/media/spiderman.420a8965.svg"
+  },
+  {
+    "revision": "1a0efd4dc6ddad9542817eec1d96ffe9",
+    "url": "/button/static/media/spinner.1a0efd4d.svg"
+  }
+]);
